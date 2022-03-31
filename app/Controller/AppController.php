42ca08527c1c2df910b0b1dd96bb0c0dd763354f
root <ysiqueira@credipronto.com.br>
2022-03-31 12:02:21 -0300

    SB-3 - Criação de funcionalidade de Autenticação

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Application level Controller
  *
@@ -18,7 +19,6 @@
  * @since         CakePHP(tm) v 0.2.9
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
-
 App::uses('Controller', 'Controller');
 
 /**
@@ -31,4 +31,26 @@ App::uses('Controller', 'Controller');
  * @link		https://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+    public $components = array(
+        'Flash',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home'),
+            'authorize' => array('Controller')
+        )
+    );
+
+    function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
+
+    public function isAuthorized($user) {
+        
+        if (isset($user['role']) && $user['role'] === 'admin') {
+            return true;
+        }
+        return false;
+    }
+
 }
--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,6 +5,19 @@ class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
     public $name = 'Posts';
 
+    public function isAuthorized($user) {
+        if (parent::isAuthorized($user)) {
+            if ($this->action === 'add') {
+                return true;
+            }
+            if (in_array($this->action, array('edit', 'delete'))) {
+                $postId = (int) $this->request->params['pass'][0];
+                return $this->Post->isOwnedBy($postId, $user['id']);
+            }
+        }
+        return false;
+    }
+
     public function index() {
         $this->set('posts', $this->Post->find('all'));
     }
@@ -15,6 +28,7 @@ class PostsController extends AppController {
 
     public function add() {
         if ($this->request->is('post')) {
+            $this->request->data['Post']['user_id'] = $this->Auth->user('id');
             if ($this->Post->save($this->request->data)) {
                 $this->Flash->success('Your post has been saved.');
                 $this->redirect(array('action' => 'index'));
